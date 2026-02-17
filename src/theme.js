4afc8361764e330a002a3b9a5197232be32b9fc6
kcprp <kp.szkola@gmail.com>
2026-02-17 10:41:59 +0000

    10.10 - specify platform specific fonts

--- src/theme.js
+++ src/theme.js
@@ -1,3 +1,5 @@
+import { Platform } from "react-native";
+
 const theme = {
   colors: {
     textPrimary: '#24292e',
@@ -12,7 +14,11 @@ const theme = {
     subheading: 16,
   },
   fonts: {
-    main: 'System',
+    main: Platform.select({
+      ios: 'Arial',
+      android: 'Roboto',
+      default: 'System',
+    }),
   },
   fontWeights: {
     normal: '400',
